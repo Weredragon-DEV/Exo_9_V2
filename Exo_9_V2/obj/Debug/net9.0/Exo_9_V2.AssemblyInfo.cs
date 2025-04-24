f1582e6eada9507726a02b96feb0a932cecd73da
Weredragon-DEV <symtrik1@gmail.com>
2025-04-24 18:47:24 -0400

    2

--- Exo_9_V2/obj/Debug/net9.0/Exo_9_V2.AssemblyInfo.cs
+++ Exo_9_V2/obj/Debug/net9.0/Exo_9_V2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo_9_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df5eec8769baeac6ce02d673b98ee6f00515d193")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo_9_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo_9_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
